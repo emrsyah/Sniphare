c2ed8b55d660baf218f5573a595113d9c19314df
Emirsyah <muhammademir48@gmail.com>
2022-07-29 23:30:12 +0700

    add init home hero ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center px-10 py-4 justify-between border-b-[1px] border-gray-700">
-      <h5 className="text-xl nunito font-semibold">Sniphare🧁</h5>
+      <Link to="/" className="text-xl nunito font-semibold">Sniphare🧁</Link>
       <div className="flex items-center gap-3">
         <button className="flex items-center gap-4 py-[6px] px-[10px] border-[1px] border-gray-500 rounded">
           <Icon icon="akar-icons:github-fill" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { Helmet } from "react-helmet";
+import Navbar from "../components/Navbar";
+import { Icon } from "@iconify/react";
 
 const Home = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Sniphare - Where Developer Find and Share Their Snippets</title>
-    </Helmet>
-    <div>
-        <Navbar/>
-    </div>
+      </Helmet>
+      <div>
+        <Navbar />
+        <div className="relative overflow-hidden">
+          <div className="h-32 w-32 bg-indigo-700 absolute blur-[100px]"></div>
+          <div className="h-48 w-48 right-4 top-20 bg-indigo-700 absolute blur-[130px]"></div>
+          <div className="flex flex-col items-center justify-center my-24">
+            <h1 className="text-6xl tracking-wide font-bold text-center">
+              Developer Snippet Homebase
+            </h1>
+            <p className="text-gray-300 mt-5 text-xl">
+              Where developer find and share their snippet, Share your snippet
+              now.
+            </p>
+            <button className="text-xl flex items-center gap-3 bg-indigo-600 hover:bg-indigo-700 py-3 px-6 mt-10 rounded font-medium">
+              <p>Start Sharing</p>
+              <Icon icon="akar-icons:arrow-right" />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
